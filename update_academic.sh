ac48116c3b5b853e83011cab75bce73b5391b518
Sky Ao <aoxiaojian@gmail.com>
2018-05-12 21:42:14 +0800

    update academic script；

--- update_academic.sh
+++ update_academic.sh
@@ -1,5 +1,12 @@
 #!/usr/bin/env bash
 
-rm -rf themes/academic
+if [ ! -d "themes" ];then
+  mkdir themes
+fi
+
+if [ -d "themes/academic" ];then
+  rm -rf themes/academic
+fi
+
 cp -r ../hugo-academic/ themes/academic
 
